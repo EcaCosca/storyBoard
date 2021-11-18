7dfdf0c25c50253ba9cfd9a4bf7a09329651e5cd
EcaCosca <enriquecoscarelli@gmail.com>
2021-11-18 10:22:16 +0300

    heroku deployment

---
+++ client/build/precache-manifest.f9d857a341cc832e838cadce3239d586.js
@@ -0,0 +1,266 @@
+self.__precacheManifest = [
+  {
+    "revision": "32827342a855e504e59c",
+    "url": "/static/css/main.bd7d6e31.chunk.css"
+  },
+  {
+    "revision": "32827342a855e504e59c",
+    "url": "/static/js/main.32827342.chunk.js"
+  },
+  {
+    "revision": "831b525dc7d0507d7cc8",
+    "url": "/static/js/1.831b525d.chunk.js"
+  },
+  {
+    "revision": "754b0d5e3d4d5a020e44",
+    "url": "/static/js/2.754b0d5e.chunk.js"
+  },
+  {
+    "revision": "ef19b4db328040bb53fe",
+    "url": "/static/js/runtime~main.ef19b4db.js"
+  },
+  {
+    "revision": "b3d599d6fd5d5fc3f8c949b46bea1ad1",
+    "url": "/static/media/background-grid.b3d599d6.jpeg"
+  },
+  {
+    "revision": "452d731a356778e6ed733be05b32ea2f",
+    "url": "/static/media/x.452d731a.svg"
+  },
+  {
+    "revision": "19ff5f446abb976169da5dbdfd5d4891",
+    "url": "/static/media/page.19ff5f44.svg"
+  },
+  {
+    "revision": "af2d4378046293a05bc5f059c49777f7",
+    "url": "/static/media/logo.af2d4378.svg"
+  },
+  {
+    "revision": "4782e19ed85b2ceb9a41db33a17f2b59",
+    "url": "/static/media/arrow.4782e19e.svg"
+  },
+  {
+    "revision": "7386609a4952a30078283897eb550785",
+    "url": "/static/media/male.7386609a.svg"
+  },
+  {
+    "revision": "f747945e9f0e72654038e4c4087b83e9",
+    "url": "/static/media/female.f747945e.svg"
+  },
+  {
+    "revision": "e41fbec17838eb73267afa06a2f08d07",
+    "url": "/static/media/border.e41fbec1.svg"
+  },
+  {
+    "revision": "557bc97bb0f628b81a39e270e25368de",
+    "url": "/static/media/facial_exp-1.557bc97b.svg"
+  },
+  {
+    "revision": "6712256127f84cf019d7d0b48d7f1be9",
+    "url": "/static/media/facial_exp-2.67122561.svg"
+  },
+  {
+    "revision": "0a710ca6130a785f789a573d57a69f3e",
+    "url": "/static/media/facial_exp-3.0a710ca6.svg"
+  },
+  {
+    "revision": "ec272d0e7c5310707e5e4e46ee83bef3",
+    "url": "/static/media/facial_exp-4.ec272d0e.svg"
+  },
+  {
+    "revision": "c2a57dd2dba17e49216fed6acbbe1328",
+    "url": "/static/media/facial_exp-5.c2a57dd2.svg"
+  },
+  {
+    "revision": "4fbc068010edf6a8547a3ddbf5ac4468",
+    "url": "/static/media/dress-1.4fbc0680.svg"
+  },
+  {
+    "revision": "53892c75678f6349ed001ce33e471bf4",
+    "url": "/static/media/dress-2.53892c75.svg"
+  },
+  {
+    "revision": "d8a0949f5741bacfde0a5a7479eec8da",
+    "url": "/static/media/dress-3.d8a0949f.svg"
+  },
+  {
+    "revision": "a318ff60080433ae5b2f13d2a1844dc7",
+    "url": "/static/media/dress-4.a318ff60.svg"
+  },
+  {
+    "revision": "c001ea20369fccd895612c4e725623e3",
+    "url": "/static/media/dress-5.c001ea20.svg"
+  },
+  {
+    "revision": "3f598fc859b8c1dae1de240c2590ef4e",
+    "url": "/static/media/hair-1.3f598fc8.svg"
+  },
+  {
+    "revision": "1da200feffec9c73720ac4e172d2af05",
+    "url": "/static/media/hair-2.1da200fe.svg"
+  },
+  {
+    "revision": "47ae5ed4ca5193f133d4361514943585",
+    "url": "/static/media/hair-3.47ae5ed4.svg"
+  },
+  {
+    "revision": "7f72e9f7f62e4d5bb52ca7d9dc711587",
+    "url": "/static/media/hair-4.7f72e9f7.svg"
+  },
+  {
+    "revision": "9c3ce623d54c3c7b5e77c2c24ab4c228",
+    "url": "/static/media/hair-5.9c3ce623.svg"
+  },
+  {
+    "revision": "d35d1a96ab34dd4b5d614e88bd9b8299",
+    "url": "/static/media/back_hair.d35d1a96.svg"
+  },
+  {
+    "revision": "fef42939350ec4c78781a99195881fae",
+    "url": "/static/media/back_hair_2.fef42939.svg"
+  },
+  {
+    "revision": "4fc5fb07ac60dc27c7cb3a280d43db5d",
+    "url": "/static/media/none.4fc5fb07.svg"
+  },
+  {
+    "revision": "4c18c5754ce59b45606c76e414fb06de",
+    "url": "/static/media/shoes-1.4c18c575.svg"
+  },
+  {
+    "revision": "3038faa96c1777024f6db71f73d8788f",
+    "url": "/static/media/shoes-2.3038faa9.svg"
+  },
+  {
+    "revision": "6f942797e8d97dd11517b7ae5da1970a",
+    "url": "/static/media/shoes-3.6f942797.svg"
+  },
+  {
+    "revision": "4e9656613165f382db5fbacb2a16d1de",
+    "url": "/static/media/shoes-4.4e965661.svg"
+  },
+  {
+    "revision": "e3d8aa6b0689e26e8384a140bc288a95",
+    "url": "/static/media/shoes-5.e3d8aa6b.svg"
+  },
+  {
+    "revision": "4eeb06d84090b6f921257b1acac9c2a6",
+    "url": "/static/media/facial_exp_1.4eeb06d8.svg"
+  },
+  {
+    "revision": "be83f6baa63be8d183ab22e9597d90ff",
+    "url": "/static/media/facial_exp_2.be83f6ba.svg"
+  },
+  {
+    "revision": "b2630c8b0224908081345c0c2f3cf769",
+    "url": "/static/media/facial_exp_3.b2630c8b.svg"
+  },
+  {
+    "revision": "b1a29400e437fb514e38fa4c366a15e3",
+    "url": "/static/media/facial_exp_4.b1a29400.svg"
+  },
+  {
+    "revision": "03a67747e7437fb599542d53b3a1719f",
+    "url": "/static/media/facial_exp_5.03a67747.svg"
+  },
+  {
+    "revision": "516b2f43b81f7569d1f8b36a50b3ac64",
+    "url": "/static/media/hair_1.516b2f43.svg"
+  },
+  {
+    "revision": "67731feaf5127ff2a37bc52c312be812",
+    "url": "/static/media/hair_2.67731fea.svg"
+  },
+  {
+    "revision": "fdd49c4ddf17c12205f08e2308eff919",
+    "url": "/static/media/hair_3.fdd49c4d.svg"
+  },
+  {
+    "revision": "842cac0b9dc2150a32c801bb125586f8",
+    "url": "/static/media/hair_4.842cac0b.svg"
+  },
+  {
+    "revision": "11c6de16bfc1bff73d7cbba7e59eaa93",
+    "url": "/static/media/hair_5.11c6de16.svg"
+  },
+  {
+    "revision": "4f7185d38015eb7a626a3203efbc7cdd",
+    "url": "/static/media/shoes_1.4f7185d3.svg"
+  },
+  {
+    "revision": "e7e598dd4ea910d5d7471d8a496e836f",
+    "url": "/static/media/shoes_2.e7e598dd.svg"
+  },
+  {
+    "revision": "a43414ccf13e5f66a1edcf6e04fb4aca",
+    "url": "/static/media/shoes_3.a43414cc.svg"
+  },
+  {
+    "revision": "cb554eb3cebf442573075de889dcb39e",
+    "url": "/static/media/shoes_4.cb554eb3.svg"
+  },
+  {
+    "revision": "4f6862bb4424e0af98391cde0da94be7",
+    "url": "/static/media/shirt_1.4f6862bb.svg"
+  },
+  {
+    "revision": "dbb18ba340d7196eef3424dfb49d3853",
+    "url": "/static/media/shirt_2.dbb18ba3.svg"
+  },
+  {
+    "revision": "e15f8bce75e071452938c8d1ef2a294f",
+    "url": "/static/media/shirt_3.e15f8bce.svg"
+  },
+  {
+    "revision": "64d8abe7b58bc1c3e813c6d624e5d92a",
+    "url": "/static/media/shirt_4.64d8abe7.svg"
+  },
+  {
+    "revision": "36208b0b7edd1fdc72d28f34074ae5b3",
+    "url": "/static/media/shirt_5.36208b0b.svg"
+  },
+  {
+    "revision": "1263d7052f7d13e04198255fb71d0ecd",
+    "url": "/static/media/pants_1.1263d705.svg"
+  },
+  {
+    "revision": "099c725d4ac89073a9442ccb98f9095d",
+    "url": "/static/media/pants_2.099c725d.svg"
+  },
+  {
+    "revision": "72f128489d8e25acda66c57d11ed7ad1",
+    "url": "/static/media/pants_3.72f12848.svg"
+  },
+  {
+    "revision": "6b14a8061a03fbf8d0106e71ae80eb7a",
+    "url": "/static/media/pants_4.6b14a806.svg"
+  },
+  {
+    "revision": "067cff4fc2b5d56bfc02e8bceb1605e2",
+    "url": "/static/media/beard_1.067cff4f.svg"
+  },
+  {
+    "revision": "998a6fd9886783ab208ef867d70e23a3",
+    "url": "/static/media/beard_2.998a6fd9.svg"
+  },
+  {
+    "revision": "bafecedb1ccb0a9372a76ff9f572be4d",
+    "url": "/static/media/beard_3.bafecedb.svg"
+  },
+  {
+    "revision": "9368a8e49501e8b88ec75120c52623b6",
+    "url": "/static/media/beard_4.9368a8e4.svg"
+  },
+  {
+    "revision": "86387434386629e0ac2006d1fd14a07e",
+    "url": "/static/media/text.86387434.svg"
+  },
+  {
+    "revision": "8ad752d00e0324f02b965c75a0b8065e",
+    "url": "/static/media/filter.8ad752d0.svg"
+  },
+  {
+    "revision": "fcd4a34257665cf12c3e39f797e9fcc1",
+    "url": "/index.html"
+  }
+];
