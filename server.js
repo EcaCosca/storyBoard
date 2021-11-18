3980ada5b1a25896c7fd52b8489b87beed2e823a
EcaCosca <enriquecoscarelli@gmail.com>
2021-11-18 11:31:25 +0300

    heroku deployment emad

--- server.js
+++ server.js
@@ -17,15 +17,25 @@ app.use('/users',usersRou)
 app.use('/library',libraryRou)
 
 
-// Serve sstatic assets if in production
+// // Serve sstatic assets if in production
 // if (process.env.NODE_ENV === 'production') {
-    // Set static folder
-    app.use(express.static('client/build'));
+//     // Set static folder
+//     app.use(express.static('client/build'));
 
 //     app.get('*', (req, res) => {
 //         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 //     });
 // }
+
+// front end
+if(fs.existsSync(path.join(__dirname, './client/build'))) {
+
+    app.use(express.static(path.join(__dirname, './client/build')));
+    app.get('/*', function (req, res) {
+      res.sendFile(path.join(__dirname, './client/build', 'index.html'));
+    });
+    
+  }
 
 
 app.listen(PORT, function () {
